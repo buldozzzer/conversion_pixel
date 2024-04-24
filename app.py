5bf0e002937f65e7c3578be0560fa939b55601a9
buldozzzer <1and130699@gmial.com>
2024-04-24 15:42:05 +0300

    init commit

---
+++ app.py
@@ -0,0 +1,70 @@
+from clickhouse_client import ClickHouseClient
+from datetime import datetime, timezone
+from aiohttp import web
+import configparser
+import logging
+import json
+import os
+
+config = configparser.ConfigParser
+config.read(os.environ.get("ABUSE_CONFIG_FILE", "config.ini"))
+logging.basicConfig(
+    format="%(name)s %(asctime)s %(levelname)s %(message)s",
+    level=logging.getLevelName(config["logging"]["level"]),
+)
+
+logging.info("Serivce started...")
+
+
+async def post_impressions(request: web.Request):
+    try:
+        data = request.json()
+        client = ClickHouseClient(
+            host=config["clickhouse"]["host"], username=config["clickhouse"]["username"]
+        )
+        # data = [(data["click_id"],
+        #         data["os"],
+        #         data["browser"],
+        #         data["country"],
+        #         data["language"],
+        #         data["compaign_id"],
+        #         data["source_id"],
+        #         data["zone_id"])]
+        data = [data]
+        response_obj = {"status": "success"}
+        return web.Response(text=json.dumps(response_obj), status=200)
+    except Exception as ex:
+        logging.error(ex)
+        response_obj = {"status": "failed", "reason": str(ex)}
+        return web.Response(text=json.dumps(response_obj), status=500)
+
+
+async def post_conversions(request: web.Request):
+    try:
+        data = request.json()
+        client = ClickHouseClient(
+            host=config["clickhouse"]["host"], username=config["clickhouse"]["username"]
+        )
+        # data = [(data["click_id"],
+        #         data["date_time"],
+        #         data["revenue"],
+        #         data["var"])]
+        data = [data]
+        for d in data:
+            d["date_time"] = datetime.now(tz=timezone.utc).strftime("%Y-%m-%d %H:%M:%S")
+        client.insert_conversions(data)
+        response_obj = {"status": "success"}
+        return web.Response(text=json.dumps(response_obj), status=200)
+    except Exception as ex:
+        logging.error(ex)
+        response_obj = {"status": "failed", "reason": str(ex)}
+        return web.Response(text=json.dumps(response_obj), status=500)
+
+
+app = web.Application()
+
+app.router.add_post("/impressions", post_impressions)
+
+app.router.add_post("/conversions", post_impressions)
+
+web.run_app(app)
---
+++ clickhouse_client.py
@@ -0,0 +1,104 @@
+import clickhouse_connect
+import logging
+
+
+class ClickHouseClient:
+    __table_impressions: str = "impressions"
+    __table_conversions: str = "conversions"
+
+    def __init__(
+        self,
+        host: str,
+        username: str,
+        password: str = "",
+        port: int = 8123,
+        db: str = "default",
+    ) -> None:
+        self.__host = host
+        self.__port = port
+        self.__username = username
+        self.__password = password
+        self.__db = db
+
+        self.__client = clickhouse_connect.get_client(
+            host=self.__host,
+            port=self.__port,
+            username=self.__username,
+            password=self.__password,
+        )
+
+        self.init_table()
+
+    def __del__(self):
+        self.__client.close()
+
+    def init_table(self):
+        self.__client.command(
+            """CREATE TABLE IF NOT EXISTS {}.{} 
+            (
+                click_id String PRIMARY KEY, 
+                os String, 
+                browser String,
+                country String,
+                language String,
+                compaign_id String,
+                source_id String,
+                zone_id String
+            )
+            ENGINE = MergeTree ORDER BY click_id
+            """.format(
+                self.__db, self.__table_impressions
+            )
+        )
+
+        self.__client.command(
+            """CREATE TABLE IF NOT EXISTS {}.{} 
+            (   
+                click_id String PRIMARY KEY,
+                date_time DateTime('Europe/London'),
+                revenue String,
+                var String
+            )
+            ENGINE = MergeTree ORDER BY date_time
+            """.format(
+                self.__db, self.__table_conversions
+            )
+        )
+
+    def drop_table(self):
+        self.__client.command(
+            "DROP TABLE IF EXISTS {}.{}".format(self.__db, self.__table_impressions)
+        )
+        self.__client.command(
+            "DROP TABLE IF EXISTS {}.{}".format(self.__db, self.__table_conversions)
+        )
+
+    def insert_impressions(self, rows: list):
+        written_rows = self.__client.insert(
+            table=self.__table_conversions,
+            data=rows,
+            column_names=[
+                "click_id",
+                "os",
+                "browser",
+                "country",
+                "language",
+                "compaign_id",
+                "source_id",
+                "zone_id",
+            ],
+        ).written_rows
+        logging.debug(f"Inserted {len(rows)} row(s)")
+
+    def insert_conversions(self, rows: list):
+        written_rows = self.__client.insert(
+            table=self.__table_conversions,
+            data=rows,
+            column_names=[
+                "click_id",
+                "date_time",
+                "revenue",
+                "var",
+            ],
+        ).written_rows
+        logging.debug(f"Inserted {len(rows)} row(s)")
